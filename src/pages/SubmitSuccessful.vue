a19ea9f99c55db339a1ac6620cfec0539c0dd31b
Carsten Bleek <bleek@cross-solution.de>
2021-05-06 21:11:13 +0200

    I wanted to preview and fixed the following
    /home/cbleek/applicationform/src/i18n/de.js
      4:2  error  Missing semicolon  semi
    
    /home/cbleek/applicationform/src/i18n/en.js
      64:2  error  Missing semicolon  semi
    
    /home/cbleek/applicationform/src/pages/Error404.vue
      4:37  error  Unexpected non-translated string used  vue/no-bare-strings-in-template
      8:50  error  Unexpected non-translated string used  vue/no-bare-strings-in-template
    
    /home/cbleek/applicationform/src/pages/SubmitSuccessful.vue
      4:70  error  Unexpected non-translated string used  vue/no-bare-strings-in-template
      5:23  error  Unexpected non-translated string used  vue/no-bare-strings-in-template
    
    ✖ 6 problems (6 errors, 0 warnings)
      2 errors and 0 warnings potentially fixable with the `--fix` option.

--- src/pages/SubmitSuccessful.vue
+++ src/pages/SubmitSuccessful.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="flex flex-center" padding>
     <q-card flat bordered style="min-width: 30px;">
-      <q-card-section class="text-h6 bg-positive text-white q-py-sm">Success</q-card-section>
+      <q-card-section class="text-h6 bg-positive text-white q-py-sm">{{ $t('success') }}</q-card-section>
       <q-card-section>
-        Ihre Bewerbung wurde erfolgreich versendet. Vielen Dank!
+        {{ $t('applicationWasSent') }}
       </q-card-section>
     </q-card>
   </q-page>
