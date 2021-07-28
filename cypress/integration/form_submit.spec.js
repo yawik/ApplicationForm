4209388d80da8f96574a851479181f3c504ffe9b
Carsten Bleek <bleek@cross-solution.de>
2021-07-28 13:47:28 +0200

    chore: separate test into different steps

--- cypress/integration/form_submit.spec.js
+++ cypress/integration/form_submit.spec.js
@@ -7,15 +7,16 @@
 
 describe('Filling and sending the form', () =>
 {
-  it('successfully loads', () =>
-  {
+  before(() => {
     cy.intercept(Cypress.env('YAWIK_JOB_DETAIL_URL') + '*', { fixture: 'job_info.json' }).as('getJobDetails');
-    cy.intercept(Cypress.env('YAWIK_APPLICATION_FORM_ACTION'), { body: { success: true } }).as('submitForm');
-    // open our app
+    //cy.viewport(1024, 768);
     cy.visit('/en?job=sw-devel');
     // load job details
     cy.wait(['@getJobDetails']);
-    // fill step 1
+  });
+
+  it('Step 1. Fill personal Data', () => {
+    // fill step 1 (personal data)
     cy.get('[name="salutation"]').first().parents('[role="radio"]').click();
     cy.get('[name="fname"]').type('Ivaylo');
     cy.get('[name="lname"]').type('Gelov');
@@ -27,10 +28,16 @@ describe('Filling and sending the form', () =>
     cy.get('[name="email"]').type('tmcdos@abv.bg');
     cy.get('[name="email"]').should('have.value', 'tmcdos@abv.bg')
     cy.get('[name="next"]').click();
-    // fill step 2
+  });
+
+  it('Step 2. Fill cover letter', () => {
+    // fill step 2 (cover letter)
     cy.get('[name="letter"]').type('My cover letter');
     cy.get('[name="next"]').click();
-    // fill step 3
+  });
+
+  it('Step 3. Answer questions', () => {
+    // fill step 3 (questions)
     cy.get('[name="begin"]').type('15-05-2021');
 
     cy.get('[name="salary_period"]').parents('label').click();
@@ -41,14 +48,21 @@ describe('Filling and sending the form', () =>
 
     cy.get('[name="salary_amount"]').type('38.60');
     cy.get('[name="next"]').click();
-    // skip step 4
+  });
+
+  it('Step 4. Add attachments', () => {
+    // skip step 4 (attachments)
     cy.get('[name="next"]').click();
-    // fill step 5
+  });
+
+  it('Step 5. Accept Privacy', () => {
+    cy.intercept(Cypress.env('YAWIK_APPLICATION_FORM_ACTION'), { body: { success: true } }).as('submitForm');
+    // fill step 5 (privacy)
     cy.get('[name="copy"]').parents('.q-checkbox').click();
     cy.get('[name="terms"]').parent().click();
     // submit form
     cy.get('[name="next"]').click();
-    cy.wait(['@submitForm']);
+    cy.wait('@submitForm');
     cy.get('.form_submit_success');
   });
 });
